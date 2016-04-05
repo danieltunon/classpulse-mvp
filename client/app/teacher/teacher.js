8fc8f42c3aa034462f422d1e06d21d06bc22b75a
Danny Tunon <danieltunon@gmail.com>
2016-04-05 13:50:42 -0700

    Add css styling and teacher view

--- client/app/teacher/teacher.js
+++ client/app/teacher/teacher.js
@@ -1,11 +1,28 @@
 angular.module('classPulse.teacher', [])
 
 .controller('TeacherController', function($scope, $rootScope, $window) {
+  $scope.quiz = {
+    question: '',
+    answers: []
+  };
+  $scope.correctAnswer;
+  $scope.newAnswer = '';
   $scope.responses = {};
 
   $scope.test = function() {
     console.log('emit as teacher');
     $rootScope.socket.emit('quiz', {question: 'test q', answers: ['first', 'second', 'third']});
+  };
+
+  $scope.addAnswer = function() {
+    $scope.quiz.answers.push($scope.newAnswer);
+    $scope.newAnswer = '';
+  };
+
+  $scope.markCorrect = function(answer) {
+    console.log('marked');
+    $scope.correctAnswer = answer;
+    console.log($scope.correctAnswer);
   };
 
   $rootScope.socket.on('newStudentResponse', function(response) {
