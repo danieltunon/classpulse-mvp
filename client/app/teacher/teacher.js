00523b7206a332d0865aa3710b624719a8a25bfd
Danny Tunon <danieltunon@gmail.com>
2016-04-05 14:43:40 -0700

    Add functionality to teacher view

--- client/app/teacher/teacher.js
+++ client/app/teacher/teacher.js
@@ -1,6 +1,8 @@
 angular.module('classPulse.teacher', [])
 
 .controller('TeacherController', function($scope, $rootScope, $window) {
+  $scope.creatingQuiz = false;
+  $scope.quizSubmitted = false;
   $scope.quiz = {
     question: '',
     answers: []
@@ -14,15 +16,28 @@ angular.module('classPulse.teacher', [])
     $rootScope.socket.emit('quiz', {question: 'test q', answers: ['first', 'second', 'third']});
   };
 
+  $scope.showCreate = function() {
+    $scope.creatingQuiz = true;
+  };
+
   $scope.addAnswer = function() {
     $scope.quiz.answers.push($scope.newAnswer);
     $scope.newAnswer = '';
   };
 
   $scope.markCorrect = function(answer) {
-    console.log('marked');
     $scope.correctAnswer = answer;
-    console.log($scope.correctAnswer);
+    // $('');
+  };
+
+  $scope.removeAnswer = function(answer) {
+    $scope.quiz.answers.splice(answer, 1);
+  };
+
+  $scope.submitQuiz = function() {
+    $rootScope.socket.emit('quiz', $scope.quiz);
+    $scope.quizSubmitted = true;
+    $scope.creatingQuiz = false;
   };
 
   $rootScope.socket.on('newStudentResponse', function(response) {
